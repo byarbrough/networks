e861652fb3e12d2b320ff9a1efc3e555e336a9fc
Brian Yarbrough <C16Brian.Yarbrough@usafa.edu>
2015-08-17 17:58:48 -0600

    updated coments

--- MastermindPEX1/MastermindClient.py
+++ MastermindPEX1/MastermindClient.py
@@ -1,10 +1,14 @@
-# UDPCLient.py
+# MastermindClient.py
 #
-# Author: Wayne Brown, August 2015
+# Author: Brian Yarbrough
+# PEX1, CS 467, USAFA
+# August 2015
+#
+# Based on UDPClient by Wayne Brown
 
 """
-    A demonstration of a typical client using the UDP protocol. A client typically runs
-    for a short time. The client must know the host name and process port number that
+    The client for playing the game. It runs for a short time.
+    The client must know the host name and process port number that
     it wants to communicate with. Notice that these values match the server program.
 """
 
@@ -12,30 +16,25 @@
 # See https://docs.python.org/3/library/socket.html
 import socket
 
-# Create a new socket to communicate with a remote server
-#   AF_INET means we want an IPv4 protocol
-#   SOCK_DGRAM means we want to send UDP datagrams
-# The socket is assigned a random port number. Clients typically have random port numbers.
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Create a new socket on random port
+c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Send a request to the server.
-# The first parameter is a byte array. The prefix b' makes the string a byte array
-# instead of a unicode string.
-# The second parameter is the address of the server as a tuple: (address, port_number)
 server_name = '127.0.0.1' # localhost
-server_port = 1055
-message = b'request'
-my_socket.sendto(message, (server_name, server_port))
+server_port = 1055 #same machine
 
-# Wait for the response from the server - this blocks until the server responds.
-# The parameter is the maximum size of the receive buffer, in bytes.
+message = b'this is the request'
+c_socket.sendto(message, (server_name, server_port))
+
+# Wait for the response from the server; max buffer size
 buffer_size = 4096
-(response, server_address) = my_socket.recvfrom(buffer_size)
+(response, server_address) = c_socket.recvfrom(buffer_size)
+
+#print response from server
 print("Server response: ", response)
 print("Server address:  ", server_address)
 
-# Close the socket, which releases all of the memory resources the socket used.
-my_socket.close()
+# Close the socket and delete from memory
+c_socket.close()
+del c_socket
 
-# Delete the socket from memory to again reclaim memory resources.
-del my_socket
--- MastermindPEX1/MastermindServer.py
+++ MastermindPEX1/MastermindServer.py
@@ -1,6 +1,10 @@
-# UDPServer.py
+# MastermindServer.py
 #
-# Author: Wayne Brown, August 2015
+# Author: Brian Yarbrough
+# PEX1, CS 467, USAFA
+# August 2015
+#
+# Based on UDPServer by Wayne Brown
 
 """
     A demonstration of a typical server using the UDP protocol. A server typically runs
@@ -13,36 +17,27 @@
 # See https://docs.python.org/3/library/socket.html
 import socket
 
-# Create a socket for communication.
-# The first socket is the module name. The second socket is a function call.
-#   AF_INET means we want an IPv4 protocol
-#   SOCK_DGRAM means we want to send UDP datagrams
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Create a socket: IPv4 protocol and sends UDP datagrams
+s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind the socket to listen on a particular port (0-65535).
-# Use a number greater than 1023. Ports 0-1023 are the reserved, "well known" ports.
-# The parameter is a address defined as a tuple (host, port)
+# Assign socket arbitrary port greater than 1023
 server_port = 1055
-my_socket.bind( ('', server_port) )
+s_socket.bind(('', server_port))
 
-# Define the maximum size of message that will be accepted
+# Max message size
 buffer_size = 4096
 
-# Run the server 24/7 (24 hours a day, 7 days a week)
+# Infinite loop
 while (True):
 
-    # Wait for a client to send the server a message.
-    # The parameter is the buffer size - the maximum number of bytes it can receive in one message
-    # The return values are the data buffer and the client's address: (host, port)
-    (data, client_address) = my_socket.recvfrom(buffer_size)
+    # Wait for messsage from client
+    (data, client_address) = s_socket.recvfrom(buffer_size)
     print("Message: ", data)
     print("Client address: ", client_address)
 
-    # Send a reply message to the client
-    # The first parameter must be a byte array. The prefix b' makes it a byte
-    # string (instead of a unicode string)
-    my_socket.sendto(b'server response', client_address)
+    # Reply to client
+    s_socket.sendto(b'server response', client_address)
 
-# If the above loop was not infinite, the socket's resources should be reclaimed
-my_socket.close()
-del my_socket
+# After infinite loop. Never called
+s_socket.close()
+del s_socket
