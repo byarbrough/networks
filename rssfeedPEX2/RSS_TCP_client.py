56deb6983044339e5f2c7222e0cb5ae11b7056ed
byarbrough <C16Brian.Yarbrough@usafa.edu>
2015-12-02 22:01:36 -0700

    parsing

--- rssfeedPEX2/RSS_TCP_client.py
+++ rssfeedPEX2/RSS_TCP_client.py
@@ -9,6 +9,7 @@
 import socket
 import sys
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 # ---------------------------------------------------------------------
 def main():
@@ -34,59 +35,16 @@ def main():
     my_socket.connect((server_addr, server_port))
 
     # get RSS
+    rss_raw = ""
     try:
-        receive_content_length(my_socket,path,netloc)
+        rss_raw = receive_content_length(my_socket,path,netloc)
+        print(parseXML(rss_raw))
     except socket.timeout:
         print("Did not receive response from server; server possibly down.")
     except ConnectionResetError:
         print("Error: connection to server reset")
     except:
         print("Unexpected error: ", sys.exc_info()[0])
-
-
-def receive_content_length(my_socket,path,netloc):
-
-    # The size of the TCP receive buffer
-    buffer_size = 1024
-
-    try:
-        # request RSS feed from server
-        message = "GET " + path + " HTTP/1.1\n" + "HOST: " + netloc + " \n\n"
-        my_socket.sendall(message.encode('utf-8'))
-
-        # couters
-        amount_expected = 1024
-        amount_received = 0
-        total_response = ""
-        # receive the first buffer worth
-        while amount_received < amount_expected:
-            h_response = my_socket.recv(buffer_size)
-            amount_received += len(h_response)
-            print(h_response)
-            total_response += h_response.decode('utf8', 'replace')
-
-        # get expected length from HTTP header
-        try:
-            index_content_length = total_response.index('Content-Length')
-            print('index_content_length',index_content_length)
-            nums = [int(s) for s in total_response[index_content_length:].split() if s.isdigit()]
-            content_length = nums[0]
-            print('content_length',content_length)
-            # content length minus what has already been received and header
-            amount_expected = content_length - (1020-total_response.index('\r\n\r\n'))
-            amount_received = 0
-        except ValueError:
-            print('Content-Length absent from header')
-
-        print('expected',amount_expected)
-        while amount_received < amount_expected:
-            response = my_socket.recv(buffer_size)
-            amount_received += len(response)
-            # build response from multiple packets
-            total_response += response.decode('utf8', 'replace')
-            print('recv',amount_received)
-
-        print("The total response from the server was:\n" + total_response)
 
 
     # finsih up
@@ -97,6 +55,75 @@ def receive_content_length(my_socket,path,netloc):
     # Delete the socket from memory to again reclaim memory resources.
     del my_socket
 
+def receive_content_length(my_socket,path,netloc):
+
+    # The size of the TCP receive buffer
+    buffer_size = 1024
+
+    # request RSS feed from server
+    message = "GET " + path + " HTTP/1.1\n" + "HOST: " + netloc + " \n\n"
+    my_socket.sendall(message.encode('utf-8'))
+
+    # couters
+    amount_expected = 1024
+    amount_received = 0
+    total_response = ""
+    # receive the first buffer worth
+    while amount_received < amount_expected:
+        h_response = my_socket.recv(buffer_size)
+        amount_received += len(h_response)
+        print(h_response)
+        total_response += h_response.decode('utf8', 'replace')
+
+    # get expected length from HTTP header
+    try:
+        index_content_length = total_response.index('Content-Length')
+        print('index_content_length',index_content_length)
+        nums = [int(s) for s in total_response[index_content_length:].split() if s.isdigit()]
+        content_length = nums[0]
+        print('content_length',content_length)
+        # content length minus what has already been received and header
+        head_end = total_response.index('\r\n\r\n')
+        print('head_end',head_end)
+        total_response = total_response[head_end:]
+        print(total_response)
+        amount_expected = content_length - (1020-head_end)
+        amount_received = 0
+    except ValueError:
+        print('Content-Length absent from header')
+
+    print('expected',amount_expected)
+    while amount_received < amount_expected:
+        response = my_socket.recv(buffer_size)
+        amount_received += len(response)
+        # build response from multiple packets
+        total_response += response.decode('utf8', 'replace')
+        print('recv',amount_received)
+
+    print(total_response)
+
+    return total_response
+
+def parseXML(text):
+    """ Parse a HTML/XML data string that is a typical RSS feed into
+        information about individual articles
+    :param text: A HTML/XML data string
+    :return: A list of articles. Each article is a (title, link) tuple
+    """
+    soup = BeautifulSoup(text, "html.parser")
+    articles = []
+    for oneItem in soup.findAll('item'):
+        title = oneItem.find('title').text
+        link = oneItem.find('link').text
+
+        articles.append((title, link))
+        # print(title, link)
+
+    return articles
+
+
 # ---------------------------------------------------------------------
 if __name__ == '__main__':
     main()
+# ---------------------------------------------------------------------
+
